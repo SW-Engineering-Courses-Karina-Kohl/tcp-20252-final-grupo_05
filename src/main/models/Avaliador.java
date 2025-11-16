fa8b0bad07596400fac695a0aa2810d98abe6c5f
Bruna <brunarblima93@gmail.com>
2025-11-16 03:10:15 -0300

    criação interface Avaliador

---
+++ src/main/models/Avaliador.java
@@ -0,0 +1,6 @@
+package main.models;
+
+public interface Avaliador {
+    // adicionar tipo de saída e parâmetros posteriormente pois dependem de classes ainda não criadas.
+    public void avaliar(); 
+}
