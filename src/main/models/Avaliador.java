b451eee2c207d325257d42a342e46c89acd0d62f
elano-tavares <elano.tavares.ncs@gmail.com>
2025-11-16 14:46:22 -0300

    Refatoração no tipo de retorno da interface Avaliador, antes: void. agora: Avaliacao.

--- src/main/models/Avaliador.java
+++ src/main/models/Avaliador.java
@@ -1,11 +1,12 @@
 package main.models;
 
 public interface Avaliador {
+
     /*
-     * @param conteudo(Filme, Serie, etc) a ser avaliado
-     * @param nota valor numerico para avaliacao
-     * @param comentario texto sobre o conteudo
-     * @return um objeto avaliacao contendo os dados da avaliacao
-     */
-    public void avaliar(Conteudo conteudo, int nota, String comentario); // classe Conteudo ainda não criada, em breve implementada.
+    * @param conteudo(Filme, Serie, etc) a ser avaliado
+    * @param nota valor numerico para avaliacao
+    * @param comentario texto sobre o conteudo
+    * @return um objeto avaliacao contendo os dados da avaliacao
+    */
+    Avaliacao avaliar(Conteudo conteudo, int nota, String comentario);
 }
