e4aa2eaa27f7ea9b6d7ccadd18e47be7a0e5f164
Bruna <brunarblima93@gmail.com>
2025-11-16 03:45:57 -0300

    Criação classe Arigo

---
+++ src/main/models/Arigo.java
@@ -0,0 +1,11 @@
+package main.models;
+
+import java.time.LocalDate;
+
+public class Arigo extends Pessoa{
+    
+
+    public Arigo(String nome, LocalDate dataNasc, String email){
+        super(nome, dataNasc, email);
+    }
+}
