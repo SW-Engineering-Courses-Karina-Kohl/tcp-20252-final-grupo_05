7dd65fef32d06842813ddddf3fcc7d5cbffc4b20
BrunaLimaa <brunarblima93@gmail.com>
2025-11-22 23:41:42 -0300

    Implementação do método avaliar()

--- src/main/models/Arigo.java
+++ src/main/models/Arigo.java
@@ -2,9 +2,25 @@ package main.models;
 
 import java.time.LocalDate;
 
-public class Arigo extends Pessoa {
+public class Arigo extends Pessoa implements Avaliador {
+
+    private static final int PESO_PADRAO = 1;
     
     public Arigo(String nome, LocalDate dataNasc, String email){
         super(nome, dataNasc, email);
+    }
+
+    @Override
+    public  Avaliacao avaliar(Conteudo conteudo, int nota, String comentario) {
+
+        if(nota < 1 || nota > 5) {
+            throw new IllegalArgumentException("A nota deve ser entre 1 e 5");
+        }
+
+        Avaliacao novaAvaliacao = new Avaliacao(nota, PESO_PADRAO, comentario, this.getId());
+
+        conteudo.adicionarAvaliacao(novaAvaliacao);
+
+        return novaAvaliacao;
     }
 }
