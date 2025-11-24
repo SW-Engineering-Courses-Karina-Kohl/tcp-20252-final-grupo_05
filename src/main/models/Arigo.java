5a026d37ee57eb46c734eb105cbeb705c1c2d989
JoaoDorneles <jvserpa@hotmail.com>
2025-11-24 17:53:13 -0300

    Ajustando classe arigo para avaliar um conteudo

--- src/main/models/Arigo.java
+++ src/main/models/Arigo.java
@@ -2,9 +2,20 @@ package main.models;
 
 import java.time.LocalDate;
 
-public class Arigo extends Pessoa {
+public class Arigo extends Pessoa implements Avaliador {
+
+    private static final int PESO_AVALIACAO = 1;
     
     public Arigo(String nome, LocalDate dataNasc, String email){
         super(nome, dataNasc, email);
+    }
+
+    @Override
+    public Avaliacao avaliar(Conteudo conteudo, int nota, String comentario) {
+        Avaliacao novaAvaliacao =new Avaliacao(nota, PESO_AVALIACAO, comentario, this.getId());
+
+        conteudo.adicionarAvaliacao(novaAvaliacao);
+
+        return novaAvaliacao;
     }
 }
